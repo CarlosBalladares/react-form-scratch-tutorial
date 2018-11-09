8e30f5ef3cbc0e3ecd9d8fc1b5f4ee133f2123b2
Carlos Balladares <carlos@balladares.co>
2018-11-08 23:11:49 -0800

    adding initial files

---
+++ App.js
@@ -0,0 +1,40 @@
+const Pet = (props) => {
+  return(
+    React.createElement("div", {}, [
+      React.createElement("h1", {}, props.name),
+      React.createElement("h2", {}, props.animal),
+      React.createElement("h3", {}, props.breed),
+    ])
+  );
+}
+
+class App extends React.Component{
+  handleClick(){
+    alert("hello")
+  }
+  render(){
+    return (
+      React.createElement( "div", {}, [
+          React.createElement("h1", {onClick: this.handleClick }, "adopt me"),
+          React.createElement(Pet, {
+            name:"Harry",
+            animal:"Dog",
+            breed:"Mixed"
+          }),
+          React.createElement(Pet, {
+            name:"Draco",
+            animal:"Dog",
+            breed:"Siberian Husky"
+          }),
+          React.createElement(Pet, {
+            name:"Luna",
+            animal:"Cat",
+            breed:"Unknown"
+          })
+        ]
+      )
+    );
+  }
+}
+
+ReactDOM.render(React.createElement(App), document.getElementById('root'));
